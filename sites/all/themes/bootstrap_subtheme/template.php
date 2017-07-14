68850b92bffa00b26fc4d6826a816d731431d1fb
fawwad <fawwadk@gmail.com>
2017-07-14 17:58:34 +0530

    changes form office

--- sites/all/themes/bootstrap_subtheme/template.php
+++ sites/all/themes/bootstrap_subtheme/template.php
@@ -6,7 +6,7 @@
 /* -- Delete this line if you want to use this function// */
 function bootstrap_subtheme_preprocess_page(&$variables, $hook) {
   
-   dpm($variables);
+   //dpm($variables);
   //$variables['sample_variable'] = t('Lorem ipsum.');
    //if($variables['is_front']){
 	   // $variables['title'] = '';//$variables['node']->title;
